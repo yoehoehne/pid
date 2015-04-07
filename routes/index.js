d7e4ceae0d46de6b23972e693d37193a03977b6e
Joe Hoehne <yoehoehne@gmail.com>
2015-04-07 09:57:49 -0600

    changed pid

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var pid = 5;//process.pid
+    var pid = process.pid;
     console.log("ID "+pid);
     res.render('index', { title: 'Cluster', pid:pid });
+});
+
+router.get('/pid', function(req, res, next) {
+    console.log("Service ID "+process.pid);
+    res.json({ title: 'Cluster', pid:process.pid });
 });
 
 module.exports = router;
